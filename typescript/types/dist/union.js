23c3bddcbae496afbc8c4458f30bbf3ade0f8b30
abhiav-spec <kumarabhinav6649@gmail.com>
2026-07-12 07:31:06 +0530

    intersection via extends in interface

--- typescript/types/dist/union.js
+++ typescript/types/dist/union.js
@@ -15,4 +15,29 @@ const employee1 = {
     salary: 50000,
 };
 console.log(employee1);
+const employee2 = {
+    user: "Abhinav Mishra",
+    emplyeeId: 1,
+    employeeType: "Permanent",
+    status: "Active",
+    jobTitle: "Software Engineer",
+    joiningDate: "2023-01-01",
+    position: "Developer",
+    salary: 50000,
+    address: "123 Main St, New York, NY, USA",
+};
+function printEmployeeDetails(employee) {
+    console.log(`Employee ID: ${employee.emplyeeId}`);
+    console.log(`Employee Type: ${employee.employeeType}`);
+    console.log(`Status: ${employee.status}`);
+    console.log(`Job Title: ${employee.jobTitle}`);
+    console.log(`Joining Date: ${employee.joiningDate}`);
+    console.log(`Position: ${employee.position}`);
+    console.log(`Salary: ${employee.salary}`);
+    console.log(`User: ${employee.user}`);
+    if (employee.address) {
+        console.log(`Address: ${employee.address}`);
+    }
+}
+printEmployeeDetails(employee2);
 //# sourceMappingURL=union.js.map
--- typescript/types/src/union.ts
+++ typescript/types/src/union.ts
@@ -32,3 +32,61 @@ const employee1: user & manager = {
     salary: 50000,
 };
 console.log(employee1);
+
+// interface can do intersection but not union, union can be done by type only
+//interface can extend another interface but not type, type can extend another type but not interface
+type employee = {
+    user: string;
+    address?: string;
+    emplyeeId: number;
+    employeeType: string;
+    status: string;
+    jobTitle: string;
+    joiningDate: string;
+    position: string;
+    salary: number;
+};
+
+type Manager = {
+    user: string;
+    address?: string;
+    emplyeeId: number;
+    employeeType: string;
+    status: string;
+    jobTitle: string;
+    joiningDate: string;
+    position: string;
+    salary: number;
+};
+
+
+
+interface  Employee  extends Manager , employee {}
+
+const employee2: Employee = {
+    user: "Abhinav Mishra",
+    emplyeeId: 1,
+    employeeType: "Permanent",
+    status: "Active",
+    jobTitle: "Software Engineer",
+    joiningDate: "2023-01-01",
+    position: "Developer",
+    salary: 50000,
+    address: "123 Main St, New York, NY, USA",
+};
+
+function printEmployeeDetails(employee: Employee): void {
+    console.log(`Employee ID: ${employee.emplyeeId}`);
+    console.log(`Employee Type: ${employee.employeeType}`);
+    console.log(`Status: ${employee.status}`);
+    console.log(`Job Title: ${employee.jobTitle}`);
+    console.log(`Joining Date: ${employee.joiningDate}`);
+    console.log(`Position: ${employee.position}`);
+    console.log(`Salary: ${employee.salary}`);
+    console.log(`User: ${employee.user}`);
+    if (employee.address) {
+        console.log(`Address: ${employee.address}`);
+    }
+}
+
+printEmployeeDetails(employee2);
